46828f7fe1caa573ddae21de65b7a092feb3330f
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-01-04 14:25:39 +0900

    #1 add animation
    애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -43,3 +43,51 @@ new Swiper('.notice-line .swiper-container', {
   autoplay: true,
   loop: true
 });
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click',(ev) => {
+  isHidePromotion = !isHidePromotion;
+  if(isHidePromotion) {
+    // 숨김처리
+    promotionEl.classList.add('hide');
+  } else {
+    // 공개
+    promotionEl.classList.remove('hide');
+  }
+});
+
+function random(min,max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector,delay,size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
